c7721331f1368ec5096557b880223af3a9271344
Mattia Mancini <mattia.mancini@gmail.com>
2023-08-15 17:49:17 +0100

    Bug fixed in the creation of the databases

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.config import load_config
 from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus
 
 user_db_config = load_config('user_db')
 data_db_config = load_config('data_db')
@@ -8,8 +9,10 @@ data_db_config = load_config('data_db')
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['USER_DB_URI'] = f"postgresql://{user_db_config['username']}:{user_db_config['password']}@{user_db_config['host']}/{user_db_config['db_name']}"
-app.config['DATA_DB_URI'] = f"postgresql://{data_db_config['username']}:{data_db_config['password']}@{data_db_config['host']}/{data_db_config['db_name']}"
+app.config['SQLALCHEMY_BINDS'] = {
+    'user_db': f"postgresql://{user_db_config['username']}:{quote_plus(user_db_config['password'])}@{user_db_config['host']}/{user_db_config['db_name']}",
+    'data_db': f"postgresql://{data_db_config['username']}:{quote_plus(data_db_config['password'])}@{data_db_config['host']}/{data_db_config['db_name']}"
+}
 
-user_db = SQLAlchemy(app, session_options={'bind': app.config['USER_DB_URI']})
-data_db = SQLAlchemy(app, session_options={'bind': app.config['DATA_DB_URI']})
+
+db = SQLAlchemy(app)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,21 +1,21 @@
-from app import user_db, data_db
+from app import db
 from datetime import datetime
 # from sqlalchemy import Date
 
 # User Models
-class Users(user_db.Model):
+class Users(db.Model):
+    __bind_key__ = 'user_db'
     __tablename__ = 'users'
-    __table_args__ = {'schema': 'users'}
-    id = user_db.Column(user_db.Integer, primary_key=True)
-    firstname = user_db.Column(user_db.String(200), nullable=False)
-    middlename = user_db.Column(user_db.String(200), nullable=True)
-    lastname = user_db.Column(user_db.String(200), nullable=False)
-    username = user_db.Column(user_db.String(20), nullable=False)
-    email = user_db.Column(user_db.String(120), unique=True, nullable=False)
-    image_file = user_db.Column(user_db.String(20), nullable=False, default='default.jpg')
-    dob = user_db.Column(user_db.Date, nullable=False)
-    password = user_db.Column(user_db.String(20), nullable=False)
-    created = user_db.Column(user_db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(200), nullable=False)
+    middlename = db.Column(db.String(200), nullable=True)
+    lastname = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    dob = db.Column(db.Date, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
