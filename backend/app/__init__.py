0be9dee59292f7937464d00ddc8235714836256f
Mattia Mancini <mattia.mancini@gmail.com>
2023-08-17 09:48:47 +0100

    Added user registration route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from app.config import load_config
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import quote_plus
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_cors import CORS
 
 user_db_config = load_config('user_db')
 data_db_config = load_config('data_db')
@@ -9,10 +12,18 @@ data_db_config = load_config('data_db')
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'your_secret_key'
+app.config["WTF_CSRF_ENABLED"] = False ##THIS IS ONLY FOR TESTING WITH POSTMAN!!!!
 app.config['SQLALCHEMY_BINDS'] = {
     'user_db': f"postgresql://{user_db_config['username']}:{quote_plus(user_db_config['password'])}@{user_db_config['host']}/{user_db_config['db_name']}",
     'data_db': f"postgresql://{data_db_config['username']}:{quote_plus(data_db_config['password'])}@{data_db_config['host']}/{data_db_config['db_name']}"
 }
 
+CORS(app)
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' #this sends back to the login route when trying manually to navigate to a route for only authenticated users
+login_manager.login_message_category = 'info'
+
+from app import routes
---
+++ backend/app/routes.py
@@ -0,0 +1,39 @@
+from app import app
+from flask_login.mixins import UserMixin
+from flask_login import login_user, current_user, logout_user, login_required
+from flask import flash, jsonify, request
+from flask import render_template, url_for, redirect
+from app.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
+                       RequestResetForm, ResetPasswordForm)
+from app.models import User
+from app import app, db, bcrypt
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+
+    # if current_user.is_authenticated:
+    #     pass
+    data = request.json
+    form = RegistrationForm(data=data)
+    if form.validate():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        
+        user = User(
+            firstname=form.firstname.data,
+            middlename=form.middlename.data if form.middlename.data else None,
+            lastname=form.lastname.data,
+            dob=form.dob.data,
+            username=form.username.data, 
+            email=form.email.data, 
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created. You are now able to log in', 'success')
+        return jsonify({'message': 'Registration successful'}), 200
+    return jsonify({'errors': form.errors}), 400
