276b710a22b1bed7b3f24ffc329c2c99a0f060b3
Mattia Mancini <mattia.mancini@gmail.com>
2023-08-17 09:45:54 +0100

    minor code edits in models

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,18 +3,18 @@ from datetime import datetime
 # from sqlalchemy import Date
 
 # User Models
-class Users(db.Model):
+class User(db.Model):
     __bind_key__ = 'user_db'
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(200), nullable=False)
     middlename = db.Column(db.String(200), nullable=True)
     lastname = db.Column(db.String(200), nullable=False)
-    username = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     dob = db.Column(db.Date, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(1000), nullable=False)
     created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
--- backend/initialise_db.py
+++ backend/initialise_db.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import Users
+from app.models import User
 
 if __name__ == '__main__':
     with app.app_context():
