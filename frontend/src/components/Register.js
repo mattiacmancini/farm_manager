189a0d76a09f89979b3fe857ebf8cee75cd20440
Mattia Mancini <mattia.mancini@gmail.com>
2023-08-19 17:17:39 +0100

    Registration form handling db submit and errors

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,110 +1,205 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
-export class Register extends Component {
+function Register(props) {
+  const [firstName, setFirstName] = useState('');
+  const [middleName, setMiddleName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [agreementCheck, setAgreementCheck] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [errors, setErrors] = useState({});
+  // const redirect = useNavigate();
 
-    constructor(props) {
-        super(props)
+  const validateCheckBox = (value) => {
+    return value
+  }
 
-        this.state = {
-            firstname: "",
-            lastname: "",
-            email: "",
-            username: "",
-            password: "",
-            checkbox: false
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (Object.keys(errors).length > 0) {
+      setErrors({});
+    }
+
+    if (successMessage) {
+      setSuccessMessage('')
+    }
+
+    if (errorMessage) {
+      setErrorMessage('')
+    }
+
+    const userData = {
+      'firstname': firstName,
+      'middlename': middleName,
+      'lastname': lastName,
+      'dob': dateOfBirth,
+      'username': userName,
+      'email': email,
+      'password': password,
+      'confirm_password': repeatPassword
+    }
+
+    axios.post(`${props.baseUrl}register`, userData)
+      .then(response => {
+        if (response.status === 200) {
+          setSuccessMessage('User registration successful: you can now login!')
+          console.log("User registration successful:", response.data.message);
+          // redirect('/login');
         }
-    }
+      })
+      .catch(error => {
+        if (error.response && error.response.status === 400) {
+          console.log("User registration failed:", error.response.data.errors);
+          setErrors(error.response.data.errors);
+          setErrorMessage('Error registering user: check the data you entered!')
+        } else {
+          console.error("Error registering user:", error);
+        }
+      });
+  };
 
-    firstnameHandler = (event) => {
-        this.setState({
-            firstname: event.target.value
-        })
-    }
+  const handleInputChange = (setter, value, fieldName) => {
+    setErrors(errors => ({
+      ...errors,
+      [fieldName]: "",
+    }));
 
-    lastnameHandler = (event) => {
-        this.setState({
-            lastname: event.target.value
-        })
-    }
+    setter(value);
+  };
 
-    emailHandler = (event) => {
-        this.setState({
-            email: event.target.value
-        })
-    }
 
-    usernameHandler = (event) => {
-        this.setState({
-            username: event.target.value
-        })
-    }
-    
-    passwordHandler = (event) => {
-        this.setState({
-            password: event.target.value,
-        });
-    };
+  return (
+    <React.Fragment>
+      <form className='row g-3 needs-validation' noValidate>
+        {successMessage && (
+          <div className="alert alert-success mt-3 d-flex justify-content-center align-items-center">
+            {successMessage}
+          </div>
+        )}
+        {errorMessage && (
+          <div className="alert alert-danger mt-3 d-flex justify-content-center align-items-center">
+            {errorMessage}
+          </div>
+        )}
+        <div className='col-md-5'>
+          <label htmlFor='firstNameValidator' className='form-label'>First Name</label>
+          <input
+            type='text'
+            className={`form-control ${errors['firstname'] ? 'is-invalid' : (firstName === "" ? "" : 'is-valid')}`}
+            id='firstNameValidator'
+            value={firstName}
+            onChange={(e) => handleInputChange(setFirstName, e.target.value, 'firstname')} />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">{errors['firstname']}</div>
+        </div>
+        <div className='col-md-2'>
+          <label htmlFor='middleNameValidator' className='form-label'>Middle Name</label>
+          <input
+            type='text'
+            className={`form-control ${errors['middlename'] ? 'is-invalid' : (middleName === "" ? "" : 'is-valid')}`}
+            id='middleNameValidator'
+            value={middleName}
+            onChange={(e) => handleInputChange(setMiddleName, e.target.value, 'middlename')} />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">{errors['middlename']}</div>
+        </div>
+        <div className='col-md-5'>
+          <label htmlFor='lastNameValidator' className='form-label'>Last Name</label>
+          <input
+            type='text'
+            className={`form-control ${errors['lastname'] ? 'is-invalid' : (lastName === "" ? "" : 'is-valid')}`}
+            id='lastNameValidator'
+            value={lastName}
+            onChange={(e) => handleInputChange(setLastName, e.target.value, 'lastname')} />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">{errors['lastname']}</div>
+        </div>
+        <div className='col-md-5'>
+          <label htmlFor='emailValidator' className='form-label'>Email Address</label>
+          <input
+            type='email'
+            className={`form-control ${errors['email'] ? 'is-invalid' : (email === "" ? "" : 'is-valid')}`}
+            id='emailValidator'
+            value={email}
+            onChange={(e) => handleInputChange(setEmail, e.target.value, 'email')} />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">{errors['email']}</div>
+        </div>
+        <div className='col-md-2'>
+          <label htmlFor='DoBValidator' className='form-label'>Date of Birth</label>
+          <input
+            type='date'
+            className={`form-control ${errors['dob'] ? 'is-invalid' : (dateOfBirth === "" ? "" : 'is-valid')}`}
+            id='DoBValidator'
+            value={dateOfBirth}
+            onChange={(e) => handleInputChange(setDateOfBirth, e.target.value, 'dob')} />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">{errors['dob']}</div>
+        </div>
+        <div className='col-md-5'></div>
+        <div className='col-md-4'>
+          <label htmlFor='userNameValidator' className='form-label'>Username</label>
+          <input
+            type='text'
+            className={`form-control ${errors['username'] ? 'is-invalid' : (userName === "" ? "" : 'is-valid')}`}
+            id='userNameValidator'
+            value={userName}
+            onChange={(e) => handleInputChange(setUserName, e.target.value, 'username')} />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">{errors['username']}</div>
+        </div>
+        <div className='col-md-4'>
+          <label htmlFor='passwordValidator' className='form-label'>Password</label>
+          <input
+            type='password'
+            className={`form-control ${errors['password'] ? 'is-invalid' : (password === "" ? "" : 'is-valid')}`}
+            id='passwordValidator'
+            value={password}
+            onChange={(e) => handleInputChange(setPassword, e.target.value, 'password')} />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">{errors['password']}</div>
+        </div>
+        <div className='col-md-4'>
+          <label htmlFor='repeatPasswordValidator' className='form-label'>Re-enter your password</label>
+          <input
+            type='password'
+            className={`form-control ${errors['confirm_password'] ? 'is-invalid' : (repeatPassword === "" ? "" : 'is-valid')}`}
+            id='repeatPasswordValidator'
+            value={repeatPassword}
+            onChange={(e) => handleInputChange(setRepeatPassword, e.target.value, 'confirm_password')} />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">{errors['confirm_password']}</div>
+        </div>
+        <div className="col-md-12">
+          <div className="form-check mt-3">
+            <input
+              className={`form-check-input ${validateCheckBox(agreementCheck) ? 'is-valid' : 'is-invalid'}`}
+              type="checkbox" checked={agreementCheck} onChange={(e) => handleInputChange(setAgreementCheck, e.target.checked)}
+              id="CheckBox" />
+            <label className="form-check-label" htmlFor="CheckBox">Agree to terms and conditions</label>
+            <div className="invalid-feedback">You need to agree with the T&C!</div>
+          </div>
+        </div>
+        <div className="col-12">
+          <button className="btn btn-dark mb-3 me-2" type="button" onClick={handleSubmit}>
+            Register
+          </button>
+          <Link to="/" className="btn btn-dark mb-3 me-2">
+            Back
+          </Link>
+        </div>
 
-    checkboxHandler = (event) => {
-        this.setState({ 
-            checkbox: event.target.checked 
-        });
-    }
-
-    render() {
-
-        return (
-            <React.Fragment>
-                <form className="row g-3 needs-validation" noValidate>
-                    <div className='col'></div>
-                    <div className='col-md-6'>
-                        <div className="col-md-12">
-                            <label htmlFor="validationCustomFirstname" className="form-label">First name</label>
-                            <input type="text" className="form-control" id="validationCustomFirstname" value={this.state.firstname} onChange={this.firstnameHandler} placeholder="Enter your first name" required />
-                            <div className="valid-feedback"> Looks good!</div>
-                        </div>
-                        <div className="col-md-12">
-                            <label htmlFor="validationCustomLastname" className="form-label">Last name</label>
-                            <input type="text" className="form-control" id="validationCustomLastname" value={this.state.lastname} onChange={this.lastnameHandler} placeholder="Enter your last name" required />
-                            <div className="valid-feedback">Looks good!</div>
-                        </div>
-                        <div className="col-md-12">
-                            <label htmlFor="validationCustomEmail" className="form-label">Email</label>
-                            <div className="input-group has-validation">
-                                <input type="text" className="form-control" id="validationCustomEmail" value={this.state.email} onChange={this.emailHandler} placeholder="Enter your email" aria-describedby="inputGroupPrepend" required />
-                                <div className="invalid-feedback"> Please choose a username.</div>
-                            </div>
-                        </div>
-                        <div className="col-md-12">
-                            <label htmlFor="validationCustomUsername" className="form-label">Username</label>
-                            <div className="input-group has-validation">
-                                <span className="input-group-text" id="inputGroupPrepend">@</span>
-                                <input type="text" className="form-control" id="validationCustomUsername" value={this.state.username} onChange={this.usernameHandler} placeholder="Enter a unique username" aria-describedby="inputGroupPrepend" required />
-                                <div className="invalid-feedback"> Please choose a username.</div>
-                            </div>
-                        </div>
-                        <div className="col-md-12">
-                            <label htmlFor="validationCustomPassword" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="validationCustomPassword" value={this.state.password} onChange={this.passwordHandler} placeholder="Enter your password" required />
-                            <div className="valid-feedback">Looks good!</div>
-                        </div>
-                        <div className="col-md-12">
-                            <div className="form-check mb-3 mt-3">
-                                <input className="form-check-input" type="checkbox" value={this.state.checkbox} onChange={this.checkboxHandler} id="CheckBox" required />
-                                <label className="form-check-label" htmlFor="CheckBox">Agree to terms and conditions</label>
-                            </div>
-                        </div>
-                        <div className="col-12">
-                            <button className="btn btn-dark mb-3 mt-3 me-2" type="submit">Submit form</button>
-                            <Link to="/" className="btn btn-dark mb-3 mt-3 me-2"> Back </Link>
-                        </div>
-                    </div>
-                    <div className='col'></div>
-                </form>
-            </React.Fragment>
-        )
-    }
+      </form>
+    </React.Fragment>
+  );
 }
 
-export default Register
+export default Register;
