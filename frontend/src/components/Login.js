b7acd2ab1fbea7742c39c4b941e943c4d78afb9b
Mattia Mancini <mattia.mancini@gmail.com>
2023-09-15 15:13:44 +0100

    Personalised welcome message in Dashboard

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 function Login(props) {
   const [formData, setFormData] = useState({
@@ -45,6 +46,7 @@ function Login(props) {
 
       if (response.status === 200) {
         localStorage.setItem('token', response.data.token);
+        console.log(jwt_decode(response.data.token))
         setSuccessMessage('User successfully logged-in!');
         setTimeout(() => {
           navigate(`/dashboard?message=loggedIn`); // Redirect to dashboard
@@ -62,58 +64,62 @@ function Login(props) {
 
   return (
     <div className="container">
-      {successMessage && (
-        <div className="alert alert-success">{successMessage}</div>
-      )}
-      {errorMessage && (
-        <div className="alert alert-danger">{errorMessage}</div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="text"
-            className={`form-control ${formErrors.email ? 'is-invalid' : ''}`}
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-          <div className="invalid-feedback">{formErrors.email}</div>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          {successMessage && (
+            <div className="alert alert-success">{successMessage}</div>
+          )}
+          {errorMessage && (
+            <div className="alert alert-danger">{errorMessage}</div>
+          )}
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="text"
+                className={`form-control ${formErrors.email ? 'is-invalid' : ''}`}
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+              <div className="invalid-feedback">{formErrors.email}</div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className={`form-control ${formErrors.password ? 'is-invalid' : ''}`}
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+              <div className="invalid-feedback">{formErrors.password}</div>
+            </div>
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="rememberMe"
+                name="rememberMe"
+                checked={formData.rememberMe}
+                onChange={handleInputChange}
+              />
+              <label className="form-check-label" htmlFor="rememberMe">
+                Remember me
+              </label>
+            </div>
+            <button type="submit" className="btn btn-dark mt-3 mb-3">
+              Login
+            </button>
+          </form>
+          <div className="border-top pt-3">
+            <small className="text-muted">
+              Need an account? <Link className="ml-2" to="/register">Sign Up Now</Link>
+            </small>
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className={`form-control ${formErrors.password ? 'is-invalid' : ''}`}
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-          <div className="invalid-feedback">{formErrors.password}</div>
-        </div>
-        <div className="form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="rememberMe"
-            name="rememberMe"
-            checked={formData.rememberMe}
-            onChange={handleInputChange}
-          />
-          <label className="form-check-label" htmlFor="rememberMe">
-            Remember me
-          </label>
-        </div>
-        <button type="submit" className="btn btn-dark mt-3 mb-3">
-          Login
-        </button>
-      </form>
-      <div className="border-top pt-3">
-        <small className="text-muted">
-          Need an account? <Link className="ml-2" to="/register">Sign Up Now</Link>
-        </small>
       </div>
     </div>
   );
