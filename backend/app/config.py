7bb0b19b5b4c24f4bf64ddb28ecd5104e0048ca0
Mattia Mancini <mattia.mancini@gmail.com>
2023-09-16 13:27:01 +0100

    Created AppConfig class parsing a config file

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,17 +1,42 @@
-import json
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 Mattia C. Mancini
+# Mattia C. Mancini (mattia.mancini@gmail.com), September 2023
+"""
+Read a json config file storing all the necessary variables
+and parameters for running the farm manager app
+"""
+
+import configparser
 import os
 
-def load_config(key):
-    with open('config.json') as config_file:
-        config_data = json.load(config_file)
-    return replace_placeholders(config_data)[key]
+class AppConfig:
+    """
+    Class to parse and format a configuration file containing all 
+    the necessary parameters to run the farm manager app
+    """
+    def __init__(self, config_file_path):
+            self.get_config(config_file_path)
 
-def replace_placeholders(value):
-    if isinstance(value, str):
-        return os.path.expandvars(value)
-    elif isinstance(value, dict):
-        return {k: replace_placeholders(v) for k, v in value.items()}
-    elif isinstance(value, list):
-        return [replace_placeholders(v) for v in value]
-    else:
-        return value
+    def get_config(self, config_file_path):
+        """
+        Parse items from the config.ini file
+        """
+        config = configparser.ConfigParser()
+        config.read(config_file_path)
+        for section_name in config.sections():
+            section = config[section_name]
+            section_dict = {key: os.path.expandvars(value) for key, value in section.items()}
+            setattr(self, section_name, section_dict)
+
+    def __str__(self):
+        """
+        Format and return a string representation of the AppConfig object
+        """
+        result = ""
+        for section_name in dir(self):
+            if not callable(getattr(self, section_name)) and not section_name.startswith("__"):
+                result += f"[{section_name}]\n"
+                section = getattr(self, section_name)
+                for key, value in section.items():
+                    result += f"{key} = {value}\n"
+        return result
